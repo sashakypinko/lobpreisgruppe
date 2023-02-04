31d23c2ccb792c5c124fa8df38b52de515364a04
Alexander <sashakypinkoo@gmail.com>
2023-02-04 14:26:50 +0100

    WIP

--- backend/settings.js
+++ backend/settings.js
@@ -5,7 +5,7 @@ export default {
     // dbName: 'lobpreisgruppe_production',
     apiPrefix: '/api',
     apiVersion: 'v1',
-    frontendUrl: 'http://localhost:4000',
+    frontendUrl: 'http://http://18.219.222.33:4000',
     debug: true,
     encrypting: {
       pepper: 's01lwHzfr3',
